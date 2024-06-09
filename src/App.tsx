ad869caca1606324148e551d48816261dda6de9d
Christine <2739361+christine-aqui@users.noreply.github.com>
2024-06-08 20:22:11 -0400

    code refactor and more function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-// import { useState } from 'react'
-import './App.css'
+import { Page } from "./Page/Page";
+import { AppStateProvider } from "./state/AppStateContext";
+import { Route, Routes } from "react-router-dom";
+import { Auth } from "./auth/Auth";
+import { Private } from "./auth/Private";
 
 function App() {
-
   return (
-    <> NebulaNotes </>
-  )
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+      <Route
+        path="/:id"
+        element={
+          <Private
+            component={
+              <AppStateProvider >
+                <Page />
+              </AppStateProvider>
+            }
+          />
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <Private
+            component={
+              <AppStateProvider >
+                <Page />
+              </AppStateProvider>
+            }
+          />
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
