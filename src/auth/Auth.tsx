d7bd0c3d2af56df202f9c2f4a7bff9b4c9992d59
Christine <2739361+christine-aqui@users.noreply.github.com>
2024-06-11 23:43:16 -0400

    wip adding husky

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -1,71 +1,74 @@
-import { useState, FormEvent, SetStateAction} from "react"
-import { useAuthSession } from "./AuthSessionContext"
-import { Navigate } from "react-router-dom"
-import styles from "../utils.module.css"
-import { supabase } from "../supabaseClient"
+import { useState, FormEvent, SetStateAction } from 'react';
+import { useAuthSession } from './AuthSessionContext';
+import { Navigate } from 'react-router-dom';
+import styles from '../utils.module.css';
+import { supabase } from '../supabaseClient';
 
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { ChevronRight } from "lucide-react"
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { ChevronRight } from 'lucide-react';
 
 export const Auth = () => {
-	const [loading, setLoading] = useState(false)
-	const [email, setEmail] = useState("")
-	const { session } = useAuthSession()
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const { session } = useAuthSession();
 
-	const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-		try {
-			setLoading(true)
-			const { error } = await supabase.auth.signInWithOtp({ email })
-			if(error) throw error
-			alert("Check your email for the login link!")
-		} catch (error){
-			alert(error)
-		} finally {
-			setLoading(false)
-		}
-	}
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signInWithOtp({ email });
+      if (error) throw error;
+      alert('Check your email for the login link!');
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	if(session){
-		return <Navigate to="/" />
-	}
+  if (session) {
+    return <Navigate to="/" />;
+  }
 
-	return (
-		<div className={styles.centeredFlex}>
-			<div>
-				<div className={styles.authWrapper} >
-					<h1>NebulaNotes App</h1>
-					<p>Sign in via magic link with your email below</p>
-					{loading ? ("Sending magic link...") : (
-						<form onSubmit={handleLogin}>
-							{/* <label htmlFor="email">Email: </label> */}
-							{/* <input
+  return (
+    <div className={styles.centeredFlex}>
+      <div>
+        <div className={styles.authWrapper}>
+          <h1>NebulaNotes App</h1>
+          <p>Sign in via magic link with your email below</p>
+          {loading ? (
+            'Sending magic link...'
+          ) : (
+            <form onSubmit={handleLogin}>
+              {/* <label htmlFor="email">Email: </label> */}
+              {/* <input
 								type="email"
 								id="email"
 								value={email}
 								onChange={e => setEmail(e.target.value)}
 								placeholder="Your email"
 							/> */}
-							<div className="flex w-full max-w-sm items-center space-x-2">
-							<Input 
-								type="email" 
-								placeholder="Email" 
-								value={email}  
-								onChange={(e: { target: { value: SetStateAction<string> } }) => setEmail(e.target.value)}
-							/>
-							<Button >
-								{/* Send magic link!  */}
-								<ChevronRight className="h-1 w1" />
-							</Button>
-							</div>
-						</form>
-					)}
-				</div>
-				<img className={styles.landerImage} src="/assets/img/lander.png" ></img>
-			</div>
-		</div>
-	)
-
-}
+              <div className="flex w-full max-w-sm items-center space-x-2">
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e: {
+                    target: { value: SetStateAction<string> };
+                  }) => setEmail(e.target.value)}
+                />
+                <Button>
+                  {/* Send magic link!  */}
+                  <ChevronRight className="h-1 w1" />
+                </Button>
+              </div>
+            </form>
+          )}
+        </div>
+        <img className={styles.landerImage} src="/assets/img/lander.png"></img>
+      </div>
+    </div>
+  );
+};
